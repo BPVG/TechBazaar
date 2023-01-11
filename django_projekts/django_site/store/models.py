09a07bd3479a011a6cff5677169b03a9a92fd87d
ovxon <67020288+ovxon@users.noreply.github.com>
2023-01-11 11:58:45 +0200

    progress

--- django_projekts/django_site/store/models.py
+++ django_projekts/django_site/store/models.py
@@ -1,6 +1,17 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from PIL import Image
 
-# Create your models here.
+
+def validate_image(image):
+    try:
+        img = Image.open(image)
+    except Exception as e:
+        raise ValidationError("Invalid image, please upload a different image")
+    return None
 
 class Listing(models.Model):
   listingname = models.CharField(max_length=255)
+  location = models.Charfield(max_length=255)
+  listingimage = models.ImageField(upload_to'images/', validators=[validate_image])
+  price = models.MoneyField(max_digits=10, decimal_places=2, default_currency='EUR')
--- django_projekts/django_site/store/views.py
+++ django_projekts/django_site/store/views.py
@@ -2,5 +2,9 @@ from django.http import HttpResponse
 from django.template import loader
 
 def store(request):
-  template = loader.get_template('myfirst.html')
+  template = loader.get_template('alllistings.html')
   return HttpResponse(template.render())
+
+def show_side_menu(request):
+    show_menu = request.path.startswith("/menu")  # check the request's path
+    return render(request, 'alllistings.html', {'show_menu': show_menu})
