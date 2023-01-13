be08f0b8115a950ae044c9183666a43d1b186eab
ovxon <67020288+ovxon@users.noreply.github.com>
2023-01-13 13:19:37 +0200

    updates

--- django_projekts/django_site/store/models.py
+++ django_projekts/django_site/store/models.py
@@ -10,8 +10,15 @@ def validate_image(image):
         raise ValidationError("Invalid image, please upload a different image")
     return None
 
+class User(models.Model):
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+
 class Listing(models.Model):
   listingname = models.CharField(max_length=255)
-  location = models.Charfield(max_length=255)
+  description = models.CharField(max_length=1020)
+  location = models.CharField(max_length=255)
   listingimage = models.ImageField(upload_to'images/', validators=[validate_image])
   price = models.MoneyField(max_digits=10, decimal_places=2, default_currency='EUR')
+  listingowner = models.ForeignKey(User, on_delete=models.CASCADE)
+  statuscompleted = models.BooleanField(default=False)
