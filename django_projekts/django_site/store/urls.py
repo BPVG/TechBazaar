060ea4e07caf3e28e69cbaf5d793e5e680826032
ovxon <dclasher22@gmail.com>
2023-01-15 19:48:02 +0200

    update

--- django_projekts/django_site/store/urls.py
+++ django_projekts/django_site/store/urls.py
@@ -3,11 +3,14 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import ListingDetailView
+from .views import ListingCreateView
 
 app_name = 'store'
 urlpatterns = [
     path('', views.store, name='store'),
     path('listing/<int:pk>/', ListingDetailView.as_view(), name='listing_detail'),
+    path('listing/create/', ListingCreateView.as_view(), name='listing_create'),
+    path('create/', views.create_listing, name='create_listing')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
--- django_projekts/django_site/store/views.py
+++ django_projekts/django_site/store/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Listing
 from django.views.generic import DetailView
+from .forms import ListingForm
+from django.urls import reverse
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
 class ListingDetailView(DetailView):
     model = Listing
@@ -9,3 +13,23 @@ class ListingDetailView(DetailView):
 def store(request):
     object_list = Listing.objects.filter(statuscompleted = False)
     return render(request, 'alllistings.html', {'object_list': object_list})
+
+def create_listing(request):
+    form = ListingForm()
+    return render(request, 'create_listing.html', {'form': form})
+
+def create_listing(request):
+    if request.method == 'POST':
+        form = ListingForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('store:store'))
+    else:
+        form = ListingForm()
+    return render(request, 'create_listing.html', {'form': form})
+
+class ListingCreateView(CreateView):
+    model = Listing
+    fields = ['listingname', 'description', 'listingimage', 'price','currency']
+    template_name = 'listing_form.html'
+    success_url = reverse_lazy('store')
