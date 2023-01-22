a9fcd26032b2396c1256552baec49d629e219edd
ovxon <dclasher22@gmail.com>
2023-01-22 21:51:53 +0200

    oooo

--- django_projekts/django_site/store/views.py
+++ django_projekts/django_site/store/views.py
@@ -1,35 +1,19 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from .models import Listing
-from django.views.generic import DetailView
-from .forms import ListingForm
-from django.urls import reverse
-from django.views.generic import CreateView
+from django.views.generic import DetailView, CreateView
 from django.urls import reverse_lazy
-
-class ListingDetailView(DetailView):
-    model = Listing
-    template_name = 'listing_detail.html'
+from .forms import ListingForm
 
 def store(request):
     object_list = Listing.objects.filter(statuscompleted = False)
     return render(request, 'alllistings.html', {'object_list': object_list})
 
-def create_listing(request):
-    form = ListingForm()
-    return render(request, 'create_listing.html', {'form': form})
-
-def create_listing(request):
-    if request.method == 'POST':
-        form = ListingForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect(reverse('store:store'))
-    else:
-        form = ListingForm()
-    return render(request, 'create_listing.html', {'form': form})
+class ListingDetailView(DetailView):
+    model = Listing
+    template_name = 'listing_detail.html'
 
 class ListingCreateView(CreateView):
     model = Listing
-    fields = ['listingname', 'description', 'listingimage', 'price','currency']
-    template_name = 'listing_form.html'
-    success_url = reverse_lazy('store')
+    template_name = 'create_listing.html'
+    form_class = ListingForm
+    success_url = reverse_lazy('store:store')
