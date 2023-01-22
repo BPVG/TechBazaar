5160e66c71438530ac7710283aa9d7628e288d52
ovxon <dclasher22@gmail.com>
2023-01-22 23:19:08 +0200

    Revert "nop"
    This reverts commit 796f74f2a99ba41ce440ccca0d84f7e4cb60d22d.

--- django_projekts/django_site/django_site/settings.py
+++ django_projekts/django_site/django_site/settings.py
@@ -132,5 +132,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/' 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
