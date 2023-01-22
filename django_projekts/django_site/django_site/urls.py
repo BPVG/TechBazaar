8b56a080ce077b3521179edbac1f0e4c59b3f107
ovxon <dclasher22@gmail.com>
2023-01-22 22:48:08 +0200

    Revert "oooo"
    This reverts commit a9fcd26032b2396c1256552baec49d629e219edd.

--- django_projekts/django_site/django_site/urls.py
+++ django_projekts/django_site/django_site/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from store import views
+from django.urls import include, path
 
 urlpatterns = [
-    path('', views.store, name='store'),
+    path('', include('store.urls')),
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
 ]
