72f84880a053f9ff4cd16d9cfc0bc33c3c4df9cb
ovxon <dclasher22@gmail.com>
2023-01-22 22:52:25 +0200

    Revert "Revert "Revert "oooo"""
    This reverts commit 5b362ee89f553ddc40ce401e618f570ce0b615ff.

--- django_projekts/django_site/store/models.py
+++ django_projekts/django_site/store/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.core.exceptions import ValidationError
 from PIL import Image
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
-from django.contrib.auth.models import Group, Permission
 
 def validate_image(image):
     try:
@@ -11,6 +9,10 @@ def validate_image(image):
         raise ValidationError("Invalid image, please upload a different image")
     return None
 
+class User(models.Model):
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+
 class Listing(models.Model):
   listingname = models.CharField(max_length=255)
   description = models.TextField(max_length=2000, default="Nav apraksta")
@@ -18,69 +20,3 @@ class Listing(models.Model):
   price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
   currency = models.CharField(max_length=255, default="EUR")
   statuscompleted = models.BooleanField(default=False)
-
-class UserManager(BaseUserManager):
-    def create_user(self, email, password=None):
-        """
-        Creates and saves a User with the given email and password.
-        """
-        if not email:
-            raise ValueError('Users must have an email address')
-
-        user = self.model(
-            email=self.normalize_email(email),
-        )
-
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_staffuser(self, email, password):
-        """
-        Creates and saves a staff user with the given email and password.
-        """
-        user = self.create_user(
-            email,
-            password=password,
-        )
-        user.is_staff = True
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, password):
-        """
-        Creates and saves a superuser with the given email and password.
-        """
-        user = self.create_user(
-            email,
-            password=password,
-        )
-        user.is_staff = True
-        user.is_superuser = True
-        user.save(using=self._db)
-        return user
-
-class User(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(
-        verbose_name='email address',
-        max_length=255,
-        unique=True,
-    )
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
-    groups = models.ManyToManyField(Group, related_name='users', blank=True)
-    user_permissions = models.ManyToManyField(Permission, related_name='users', blank=True)
-    objects = UserManager()
-
-    USERNAME_FIELD = 'email'
-
-    
-    groups = models.ManyToManyField(Group, related_name='store_user_set', blank=True)
-    user_permissions = models.ManyToManyField(Permission, related_name='store_user_set', blank=True)
-
-class Admin(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='admin')
-
-class Guest(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='guest')
--- django_projekts/django_site/store/views.py
+++ django_projekts/django_site/store/views.py
@@ -1,19 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Listing
-from django.views.generic import DetailView, CreateView
-from django.urls import reverse_lazy
+from django.views.generic import DetailView
 from .forms import ListingForm
-
-def store(request):
-    object_list = Listing.objects.filter(statuscompleted = False)
-    return render(request, 'alllistings.html', {'object_list': object_list})
+from django.urls import reverse
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
 class ListingDetailView(DetailView):
     model = Listing
     template_name = 'listing_detail.html'
 
+def store(request):
+    object_list = Listing.objects.filter(statuscompleted = False)
+    return render(request, 'alllistings.html', {'object_list': object_list})
+
+def create_listing(request):
+    form = ListingForm()
+    return render(request, 'create_listing.html', {'form': form})
+
+def create_listing(request):
+    if request.method == 'POST':
+        form = ListingForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('store:store'))
+    else:
+        form = ListingForm()
+    return render(request, 'create_listing.html', {'form': form})
+
 class ListingCreateView(CreateView):
     model = Listing
-    template_name = 'create_listing.html'
-    form_class = ListingForm
-    success_url = reverse_lazy('store:store')
+    fields = ['listingname', 'description', 'listingimage', 'price','currency']
+    template_name = 'listing_form.html'
+    success_url = reverse_lazy('store')
