0f2eb489f703c5e4ed9b8190ddfe32f94c17e35a
ovxon <dclasher22@gmail.com>
2023-02-17 00:11:24 +0200

    yep

--- django_projekts/django_site/django_site/settings.py
+++ django_projekts/django_site/django_site/settings.py
@@ -138,7 +138,9 @@ STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
