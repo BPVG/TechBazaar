c9825a1a19faf9919454b150403f2d05be7a5531
BPVG <bruno.rivza@je.pvg.edu.lv>
2023-02-17 00:47:13 +0200

    host1

--- django_projekts/django_site/django_site/settings.py
+++ django_projekts/django_site/django_site/settings.py
@@ -138,9 +138,8 @@ STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
